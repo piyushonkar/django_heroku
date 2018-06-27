06c453226bbebc4790e17028152821fa5b706f30
ONKAR PIYUSH PRPADEEP <onkar@cse.iitb.ac.in>
2018-06-27 15:56:38 +0530

    added procfile

--- Trial/settings.py
+++ Trial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd)$15kpe*kyakr8h&^!=@@_&*yp7=yaj#uuv=b&%gz)0!z(6)#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chatbot-smartmedics.herokuapp.com']
 
 
 # Application definition
@@ -118,4 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT=os.path.join((BASE_DIR),'static')
 STATIC_URL = '/static/'
